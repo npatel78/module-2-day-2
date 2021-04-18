b6cb04769ee24a1de7952eb55fe1ae60387a076a
npatel78 <npatel78@hotmail.com>
2021-04-18 10:24:03 -0700

    Added some basic code

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Great PHP Work</title>
+</head>
+
+<body>
+    <ul>
+        <?php
+        $students = ["Amy", "Stoney", "Chris", "Bruce"];
+        foreach ($students as $student) {
+            echo "<li>$student</li>";
+        }
+        ?>
+    </ul>
+</body>
+
+</html>
